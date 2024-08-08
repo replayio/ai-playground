c6e4d40bb8afa48021315c462d7b58c18bf28adc
Chris Toshok <toshok@gmail.com>
2024-08-08 00:59:04 -0700

    oops forgot to add these

---
+++ code-monkey-ts2/instrumentation.ts
@@ -0,0 +1,97 @@
+import { trace, Span, Tracer, context } from '@opentelemetry/api';
+import { Resource } from '@opentelemetry/resources';
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { BatchSpanProcessor, ConsoleSpanExporter, NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+
+let _tracer: Tracer | null = null;
+
+export function tracer(): Tracer {
+  if (_tracer === null) {
+    console.warn("WARNING: tracer not initialized. Returning NoOp tracer.");
+    _tracer = trace.getTracer('replayio.ai_playground');
+  }
+  return _tracer;
+}
+
+export function setTracer(newTracer: Tracer): void {
+  _tracer = newTracer;
+}
+
+export function currentSpan(): Span {
+  return trace.getSpan(context.active()) ?? trace.getTracer('default').startSpan('');
+}
+
+export function initializeTracer(attributes?: Record<string, any>): void {
+  const serviceResource = new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: "ai_playground",
+  });
+
+  const extraResource = attributes ? new Resource(attributes) : Resource.empty();
+
+  let exporter: OTLPTraceExporter | ConsoleSpanExporter | null = null;
+  const apiKey = process.env.HONEYCOMB_API_KEY;
+  const otlpEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
+
+  if (apiKey && otlpEndpoint) {
+    exporter = new OTLPTraceExporter({
+      url: otlpEndpoint,
+      headers: {
+        'x-honeycomb-team': apiKey,
+      },
+    });
+  } else if (process.env.OTEL_CONSOLE_EXPORTER) {
+    exporter = new ConsoleSpanExporter();
+  }
+
+  const provider = new NodeTracerProvider({
+    resource: extraResource.merge(serviceResource),
+  });
+
+  if (exporter) {
+    const processor = new BatchSpanProcessor(exporter);
+    provider.addSpanProcessor(processor);
+  }
+
+  provider.register();
+  setTracer(trace.getTracer("replayio.ai-playground"));
+}
+
+
+export function instrument(name: string, attributes?: Record<string, any>) {
+  return function (
+    originalMethod: any,
+    context: DecoratorContext,
+  ) {
+    if (context.kind === "method") {
+        return function (...args: any[]): any {
+            const spanAttributes: Record<string, any> = {};
+
+            if (attributes) {
+                Object.assign(spanAttributes, attributes);
+            }
+
+            return tracer().startActiveSpan(name, { attributes: spanAttributes }, span => {
+                try {
+                    const rv = originalMethod.apply(this, args);
+
+                    if (rv instanceof Promise) {
+                        return rv.then((result: any) => {
+                            span.end();
+                            return result;
+                        });
+                    }
+
+                    span.end();
+                } finally {
+                    span.end();
+                }
+            });
+        };
+    }
+
+    throw new Error(`instrument decorator only supports kind='method': kind=${context.kind}`);
+  };
+}
+
+export { Tracer };
---
+++ code-monkey-ts2/tools/replace_in_file_tool.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { makeFilePath, readFileContent, writeFileContent } from './utils';
+import { instrument, currentSpan } from '../instrumentation';
+import { IOTool } from './io_tool';
+
+const schema = z.object({
+    fname: z.string().describe("The path to the file to be modified."),
+    oldText: z.string().describe("The text to be replaced."),
+    newText: z.string().describe("The text to replace with."),
+});
+
+export class ReplaceInFileTool extends IOTool {
+    name = "replace_in_file";
+    description = "Replace all occurrences of given text in a file with the replacement";
+    schema = schema;
+
+    @instrument("Tool._call", { tool: "ReplaceInFileTool" })
+    async _call({ fname, oldText, newText }: z.infer<typeof schema>): Promise<string> {
+        currentSpan().setAttributes({
+            fname,
+            oldText,
+            newText,
+        });
+
+        try {
+            const filePath = makeFilePath(fname);
+            const fileContent = (await readFileContent(filePath)).toString();
+
+            const newContent = fileContent.replaceAll(oldText, newText);
+            if (newContent !== fileContent) {
+                await writeFileContent(filePath, newContent);
+
+                await this.notifyFileModified(fname);
+            }
+            return "file successfully updated";
+        } catch (error) {
+            console.error(`Failed to update file: ${fname}`);
+            console.error(error);
+            return "failed to update file";
+        }
+    }
+}
