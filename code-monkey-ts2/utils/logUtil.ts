ac4fe2f98409eb0a576f000d5f9268388d331e81
D. Seifert <Domiii@users.noreply.github.com>
2024-08-12 06:18:00 -0700

    Small logging updates

--- code-monkey-ts2/utils/logUtil.ts
+++ code-monkey-ts2/utils/logUtil.ts
@@ -4,24 +4,40 @@ import chalk from "chalk";
 export function initPrettyLogging(): void {
   // Monkey patch console.debug
   const originalDebug = console.debug;
-  console.debug = (...args: any[]): void => {
-    const formattedArgs = args.map((arg) =>
-      // typeof arg === "string" ?  : arg
-      typeof arg === "string" ? boxen(chalk.gray(arg), { /*backgroundColor: "greenBright",*/ dimBorder: true }) : arg
-    );
+  if (process.env.NO_DEBUG) {
+    // Mute debug messages.
+    console.debug = () => {};
+  } else {
+    console.debug = (...args: any[]): void => {
+      const formattedArgs = args.map((arg) =>
+        // typeof arg === "string" ?  : arg
+        typeof arg === "string"
+          ? boxen(chalk.gray(arg), {
+              /*backgroundColor: "greenBright",*/ dimBorder: true,
+            })
+          : arg
+      );
 
-    // Call the original debug function with the colored arguments
-    return originalDebug.apply(console, formattedArgs);
-  };
+      // Call the original debug function with the colored arguments
+      return originalDebug.apply(console, formattedArgs);
+    };
+  }
 
   // Monkey patch console.log
   const originalLog = console.log;
   console.log = (...args: any[]): void => {
-    const formattedArgs = args.map((arg) =>
-      // typeof arg === "string" ?  : arg
-      typeof arg === "string" ? boxen(arg, { /*backgroundColor: "greenBright",*/ dimBorder: false }) : arg
-    );
+    const formattedArgs = args; // .map((arg) =>
+    //   // typeof arg === "string" ?  : arg
+    //   typeof arg === "string"
+    //     ? boxen(arg, { /*backgroundColor: "greenBright",*/ dimBorder: false })
+    //     : arg
+    // )
 
+    /**
+     * Draw HR.
+     * @see https://en.wikipedia.org/wiki/Box-drawing_characters.
+     */
+    originalLog("\u2501".repeat(100));
     // Call the original debug function with the colored arguments
     return originalLog.apply(console, formattedArgs);
   };
